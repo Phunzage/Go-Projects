61a415d420d999761c919367f7120212ccbb113b
Phunzage <3143884145@qq.com>
2025-04-20 18:07:00 +0800

    第十次提交，project6客户端增加更新用户名功能

--- project6/client.go
+++ project6/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -35,6 +37,20 @@ func NewClient(serverip string, serverPort int) *Client {
 
 }
 
+// 处理server回应的消息，直接显示到标准输出即可
+func (client *Client) DealResponse() {
+	// 一旦client.conn有数据，就直接copy到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+	// 等价于
+	/*
+		for {
+			buf := make()
+			client.conn.Read(buf)
+			fmt.Println(buf)
+		}
+	*/
+}
+
 func (client *Client) menu() bool {
 	var flag int
 
@@ -55,6 +71,21 @@ func (client *Client) menu() bool {
 	}
 }
 
+func (client *Client) UpdateName() bool {
+	// 提示用户输入用户名
+	fmt.Println("请输入用户名")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+
+	if err != nil {
+		fmt.Println("conn.Write err: ", err)
+		return false
+	}
+	return true
+}
+
 func (client *Client) Run() {
 	for client.flag != 0 {
 		for client.menu() != true {
@@ -69,7 +100,7 @@ func (client *Client) Run() {
 			fmt.Println("私聊模式选择")
 		case 3:
 			// 更新用户名
-			fmt.Println("更新用户名选择")
+			client.UpdateName()
 
 		}
 	}
@@ -95,6 +126,9 @@ func main() {
 		fmt.Println("链接服务器失败...")
 		return
 	}
+
+	// 单独开启一个goroutine去处理server的回执消息
+	go client.DealResponse()
 
 	fmt.Println("链接服务器成功...")
 
